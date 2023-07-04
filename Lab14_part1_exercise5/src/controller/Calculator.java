365cd9d4afc828f47571687dbc6bb418151cc745
IkmalBadrol <ikmalbadrol29@gmail.com>
2023-07-04 15:52:12 +0800

    Lab14_Part1_Exercise5

---
+++ Lab14_part1_exercise5/src/controller/Calculator.java
@@ -0,0 +1,30 @@
+package controller;
+
+/**
+ * 
+ * @author Ikmal Badrol
+ *
+ */
+public class Calculator {
+	
+	/**
+	 * A method that will get the total summation from three numbers
+	 * @param number1
+	 * @param number2
+	 * @param number3
+	 * @return summation of the three numbers
+	 * @return multiplication of the three numbers
+	 */
+	public int getSum(int number1, int number2, int number3) {
+		
+		return number1 + number2 + number3;
+		
+	}
+	
+	public int getMultiplication (int number1, int number2, int number3) {
+		
+		return number1 * number2 * number3;
+		
+	}
+
+}
